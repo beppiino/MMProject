fff3895c4db20bf54f90298e705b559d2055b631
beppiino <a.germano7@studenti.uniba.it>
2021-07-12 17:54:24 +0200

    Working changes, works on target_names, try to understand all

--- recon.py
+++ recon.py
@@ -20,6 +20,9 @@ from utils import load_metadata, load_image, download_landmarks
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import classification_report
+from time import time
 import warnings
 
 
@@ -32,38 +35,33 @@ if not os.path.exists(dst_file):
 
 nn4_small2_train = create_model()
 nn4_small2_train.load_weights('weights/nn4.small2.v1.h5')
-# try:
-#     open('weights/nn4.small2.myTrain.h5', 'r')
-#     nn4_small2_train.load_weights('weights/nn4.small2.myTrain.h5')
-# except FileNotFoundError:
-#
-# nn4_small2_train = train_model()
+
 
 
 metadata = load_metadata('images')
 
-# Initialize the OpenFace face alignment utility
+#Initialize the OpenFace face alignment utility
 alignment = AlignDlib('models/landmarks.dat')
 
-# Load an image
+#Load an image
 jc_orig = load_image(metadata[0].image_path())
 
-# Detect face and return bounding box
+#Detect face and return bounding box
 bb = alignment.getLargestFaceBoundingBox(jc_orig)
 
-# Transform image using specified face landmark indices and crop image to 96x96
+#Transform image using specified face landmark indices and crop image to 96x96
 jc_aligned = alignment.align(96, jc_orig, bb, landmarkIndices=AlignDlib.OUTER_EYES_AND_NOSE)
 
-# Show original image
+#Show original image
 plt.subplot(131)
 plt.imshow(jc_orig)
 
-# Show original image with bounding box
+#Show original image with bounding box
 plt.subplot(132)
 plt.imshow(jc_orig)
 plt.gca().add_patch(patches.Rectangle((bb.left(), bb.top()), bb.width(), bb.height(), fill=False, color='red'))
 
-# Show aligned image
+#Show aligned image
 plt.subplot(133)
 plt.imshow(jc_aligned)
 plt.show()
@@ -78,14 +76,14 @@ embedded = np.zeros((metadata.shape[0], 128))
 for i, m in enumerate(metadata):
     img = load_image(m.image_path())
     img = align_image(img)
-    # scale RGB values to interval [0,1]
+    #scale RGB values to interval [0,1]
     img = (img / 255.).astype(np.float32)
     #img = (img * 255).round().astype(np.float32)
-    # obtain embedding vector for image
+    #obtain embedding vector for image
     embedded[i] = nn4_small2_train.predict(np.expand_dims(img, axis=0))[0]
 
 
-# Verify
+#Verify
 def distance(emb1, emb2):
     return np.sum(np.square(emb1 - emb2))
 
@@ -97,13 +95,13 @@ def show_pair(idx1, idx2):
     plt.subplot(122)
     plt.imshow(load_image(metadata[idx2].image_path()));
 
-
-show_pair(0 , 1)  #Pair two images, 
+#Pair two images
+show_pair(0 , 1)
 show_pair(0, 11)
 plt.show()
 
-distances = [] # squared L2 distance between pairs
-identical = [] # 1 if same identity, 0 otherwise
+distances = [] #squared L2 distance between pairs
+identical = [] #1 if same identity, 0 otherwise
 
 num = len(metadata)
 
@@ -121,12 +119,13 @@ f1_scores = [f1_score(identical, distances < t) for t in thresholds]
 acc_scores = [accuracy_score(identical, distances < t) for t in thresholds]
 
 opt_idx = np.argmax(f1_scores)
-# Threshold at maximal F1 score
+
+#Threshold at maximal F1 score
 opt_tau = thresholds[opt_idx]
-# Accuracy at maximal F1 score
+#Accuracy at maximal F1 score
 opt_acc = accuracy_score(identical, distances < opt_tau)
 
-# Plot F1 score and accuracy as function of distance threshold
+#Plot F1 score and accuracy as function of distance threshold
 plt.plot(thresholds, f1_scores, label='F1 score')
 plt.plot(thresholds, acc_scores, label='Accuracy')
 plt.axvline(x=opt_tau, linestyle='--', lw=1, c='lightgrey', label='Threshold')
@@ -176,15 +175,18 @@ targets = np.array([m.name for m in metadata])
 
 encoder = LabelEncoder()
 encoder.fit(targets)
-# Numerical encoding of identities
+#Numerical encoding of identities
 y = encoder.transform(targets)
 
 train_idx = np.arange(metadata.shape[0]) % 2 != 0
 test_idx = np.arange(metadata.shape[0]) % 2 == 0
 
-# 50 train examples of 10 identities (5 examples each)
+print("train index", train_idx)
+print("test_index", test_idx)
+
+#410 train examples of 41 identities (10 examples each)
 X_train = embedded[train_idx]
-# 50 test examples of 10 identities (5 examples each)
+#410 test examples of 41 identities (10 examples each)
 X_test = embedded[test_idx]
 
 y_train = y[train_idx]
@@ -204,10 +206,10 @@ acc_svc = accuracy_score(y_test, svc.predict(X_test))
 
 print(f'KNN accuracy = {acc_knn}, SVM accuracy = {acc_svc}')
 
-# Suppress LabelEncoder warning
+#Suppress LabelEncoder warning
 warnings.filterwarnings('ignore')
 
-example_idx = 1
+example_idx = 3
 
 #example_image = load_image('images/Tony_Blair/Tony_Blair_0003.jpg')
 example_image = load_image(metadata[test_idx][example_idx].image_path())
@@ -221,24 +223,7 @@ plt.title(f'Recognized as {example_identity} using SVM')
 plt.gca().add_patch(patches.Rectangle((bb.left(), bb.top()), bb.width(), bb.height(), fill=False, color='red'))
 plt.show()
 
-# from sklearn.manifold import TSNE
 
-# X_embedded = TSNE(n_components=2).fit_transform(embedded)
-
-# for i, t in enumerate(set(targets)):
-#     idx = targets == t
-#     plt.scatter(X_embedded[idx, 0], X_embedded[idx, 1], label=t)
-
-# plt.legend(bbox_to_anchor=(1, 1))
-# plt.show()
-
-from sklearn.metrics import plot_confusion_matrix
-#print (plot_confusion_matrix(y_test, knn.predict(X_test)))
-
-#plot_confusion_matrix(knn, X_test, y_test, display_labels=targets, cmap=plt.cm.Blues)
-#displaySvc = plot_confusion_matrix(svc, X_test, y_test, display_labels=targets, cmap=plt.cm.Blues)
-from sklearn.metrics import classification_report
-from time import time
 
 t0 = time()
 y_pred = svc.predict(X_test)
@@ -247,47 +232,48 @@ print("done in %0.3fs" % (time() - t0))
 n_classes = metadata.shape[0]
 
 tg = [
- "Alessandro_Gassmann",
- "Ariel_Sharon",
- "Arnold_Schwarzenegger",
- "Ben_Affleck",
- "Brad_Pitt",
- "Bradley_Cooper",
- "Chris_Hemsworth",
- "Christian_DeSica",
- "Colin_Powell",
- "Daniel_Radcliffe",
- "Donald_Rumsfeld",
- "George_Clooney",
- "George_W_Bush",
- "Gerhard_Schroeder",
- "Halle_Berry",
- "Harrison_Ford",
- "Hugo_Chavez",
- "Jacques_Chirac",
- "Jennifer_Aniston",
- "Johnny_Depp",
- "Julia_Roberts",
- "KimRossi_Stuart",
- "Leonardo_Dicaprio",
- "Luca_Argentero",
- "Massimo_Boldi",
- "Matt_Damon",
- "Matthew_Lewis",
- "Michelle_Pfeiffer",
- "Monica_Bellucci",
- "Nicole_Kidman",
- "Orlando_Bloom",
- "Patrick_Dempsey",
- "Raoul_Bova",
- "Richard_Gere",
- "Robert_DownayJr",
- "Sandra_Bullock",
- "Tom_Cruise",
- "Tony_Blair",
- "Vladimir_Putin",
- "Will_Smith"
- ]
+      "Alessandro_Gassmann",
+      "Ariel_Sharon",
+      "Arnold_Schwarzenegger",
+      "Ben_Affleck",
+      "Brad_Pitt",
+      "Bradley_Cooper",
+      "Chris_Hemsworth",
+      "Christian_DeSica",
+      "Colin_Powell",
+      "Daniel_Radcliffe",
+      "Donald_Rumsfeld",
+      "George_Clooney",
+      "George_W_Bush",
+      "Gerhard_Schroeder",
+      "Halle_Berry",
+      "Harrison_Ford",
+      "Hugo_Chavez",
+      "Jacques_Chirac",
+      "Jennifer_Aniston",
+      "Johnny_Depp",
+      "Julia_Roberts",
+      "KimRossi_Stuart",
+      "Leonardo_Dicaprio",
+      "Luca_Argentero",
+      "Massimo_Boldi",
+      "Matt_Damon",
+      "Matthew_Lewis",
+      "Michelle_Pfeiffer",
+      "Monica_Bellucci",
+      "Nicole_Kidman",
+      "Orlando_Bloom",
+      "Patrick_Dempsey",
+      "Raoul_Bova",
+      "Richard_Gere",
+      "Robert_DownayJr",
+      "Sandra_Bullock",
+      "Tom_Cruise",
+      "Tony_Blair",
+      "Vladimir_Putin",
+      "Will_Smith",
+      "TEST"
+      ]
 
 print(classification_report(y_test, y_pred, target_names=tg))
 #print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
